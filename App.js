8c4662203edb9e186f05fdac52fa7c278bf085f1
Carmine Di Martino <cardimart@gmail.com>
2019-01-13 08:27:32 +0100

    Display button floating at the bottom of the page. This is an initial exploration.

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import React from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, ScrollView, View, Button } from "react-native";
+import { LinearGradient } from "expo";
 
 import FeedSuggestionBox from "./components/FeedSuggestionBox";
 
 export default class App extends React.Component {
+  openFilmSearchSection = () => {
+    console.log("\nJust testing the button\n");
+  };
+
   render() {
     return (
-      <ScrollView style={styles.feedContainer}>
-        <FeedSuggestionBox name="Stefano" />
-        <FeedSuggestionBox name="Carmine" />
-        <FeedSuggestionBox name="Martina" />
-        <FeedSuggestionBox name="Rocco" />
-        <FeedSuggestionBox name="Angelo" />
-        <FeedSuggestionBox name="Marco" />
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <ScrollView style={styles.feedContainer}>
+          <FeedSuggestionBox name="Stefano" />
+          <FeedSuggestionBox name="Carmine" />
+          <FeedSuggestionBox name="Martina" />
+          <FeedSuggestionBox name="Rocco" />
+          <FeedSuggestionBox name="Angelo" />
+          <FeedSuggestionBox name="Marco" />
+        </ScrollView>
+        <View
+          style={{
+            flex: 1,
+            position: "absolute",
+            width: "100%",
+            bottom: 50,
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center"
+          }}
+        >
+          <LinearGradient // colors={["rgba(224, 224, 224, 0)", "#E0E0E0"]}
+            colors={["black", "blue"]}
+            style={{
+              height: 80,
+              justifyContent: "center",
+              alignContent: "center"
+            }}
+          >
+            <Button
+              style={{
+                flex: 1,
+                borderRadius: 35,
+                margin: "0 auto",
+                backgroundColor: "#2EA6FF"
+              }}
+              onPress={this.openFilmSearchSection.bind(this)}
+              title="Learn More"
+              color="white"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </LinearGradient>
+        </View>
+      </View>
     );
   }
 }
