080495371f905f979ee6829d69066a2730f90a94
Carmine Di Martino <cardimart@gmail.com>
2019-01-13 09:31:26 +0100

    Implement TouchableOpacity for the Suggestion button so that a function can be triggered when tapped

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, ScrollView, View, Text } from "react-native";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity
+} from "react-native";
 import { LinearGradient } from "expo";
 
 import FeedSuggestionBox from "./components/FeedSuggestionBox";
@@ -20,59 +26,19 @@ export default class App extends React.Component {
           <FeedSuggestionBox name="Angelo" />
           <FeedSuggestionBox name="Marco" />
         </ScrollView>
-        <View
-          style={{
-            flex: 1,
-            position: "absolute",
-            width: "100%",
-            bottom: 50,
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center"
-          }}
-        >
+        <View style={styles.floatingPositionForButton}>
           <LinearGradient
-            // colors={["black", "blue"]}
-            colors={["rgba(224, 224, 224, 0)", "#E0E0E0"]}
-            style={{
-              height: 85,
-              justifyContent: "center",
-              alignContent: "center",
-              paddingTop: 7,
-              paddingRight: 36,
-              paddingLeft: 36,
-              paddingBottom: 18
-            }}
+            colors={greyGradient}
+            style={styles.gradientBehindButton}
           >
-            {/* Button */}
-            <View
-              style={
-                {
-                  flex: 1,
-                  height: 60,
-                  borderRadius: 35,
-                  justifyContent: "center",
-                  alignContent: "center",
-                  backgroundColor: "#2EA6FF",
-                  shadowOpacity: 1,
-                  shadowRadius: 8,
-                  shadowColor: "#2EA6FF",
-                  shadowOffset: { width: 0, height: 4 }
-                } // made up this //     numbers, as I can't replicate Sketch parameters
-              }
+            <TouchableOpacity
+              style={styles.blueSuggestionButton}
               onPress={this.openFilmSearchSection.bind(this)}
             >
-              <Text
-                style={{
-                  justifyContent: "center",
-                  alignSelf: "center",
-                  color: "#F8F8F8",
-                  fontSize: 20
-                }}
-              >
+              <Text style={styles.textOfBlueButton}>
                 Consiglia serie TV o film
               </Text>
-            </View>
+            </TouchableOpacity>
           </LinearGradient>
         </View>
       </View>
@@ -80,11 +46,50 @@ export default class App extends React.Component {
   }
 }
 
+const greyGradient = ["rgba(224, 224, 224, 0)", "#E0E0E0"];
+const temporaryGradient = ["black", "blue"]; // Used for testing as more visible
+
 const styles = StyleSheet.create({
   feedContainer: {
     marginTop: 90,
     padding: 8,
     backgroundColor: "#E0E0E0",
     borderRadius: 30
+  },
+  floatingPositionForButton: {
+    flex: 1,
+    position: "absolute",
+    width: "100%",
+    bottom: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center"
+  },
+  gradientBehindButton: {
+    height: 85,
+    justifyContent: "center",
+    alignContent: "center",
+    paddingTop: 7,
+    paddingRight: 36,
+    paddingLeft: 36,
+    paddingBottom: 18
+  },
+  blueSuggestionButton: {
+    flex: 1,
+    height: 60,
+    borderRadius: 35,
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "#2EA6FF",
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    shadowColor: "#2EA6FF",
+    shadowOffset: { width: 0, height: 4 }
+  },
+  textOfBlueButton: {
+    justifyContent: "center",
+    alignSelf: "center",
+    color: "#F8F8F8",
+    fontSize: 20
   }
 });
