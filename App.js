5a5b8237d5e066e4051b709a2331f9900ee0e383
Carmine Di Martino <cardimart@gmail.com>
2019-01-13 09:14:02 +0100

    Improve styling of the Suggest button by adding blue shadow, aligning the text vertically and horizontally and adjusting linear gradients and dimensions

--- App.js
+++ App.js
@@ -32,32 +32,42 @@ export default class App extends React.Component {
           }}
         >
           <LinearGradient
-            colors={["black", "blue"]} // colors={["rgba(224, 224, 224, 0)", "#E0E0E0"]}
+            // colors={["black", "blue"]}
+            colors={["rgba(224, 224, 224, 0)", "#E0E0E0"]}
             style={{
-              height: 80,
+              height: 85,
               justifyContent: "center",
-              alignContent: "center"
+              alignContent: "center",
+              paddingTop: 7,
+              paddingRight: 36,
+              paddingLeft: 36,
+              paddingBottom: 18
             }}
           >
             {/* Button */}
             <View
-              style={{
-                flex: 1,
-                height: 60,
-                borderRadius: 35,
-                justifyContent: "center",
-                alignContent: "center",
-                backgroundColor: "#2EA6FF",
-                color: "#F8F8F8"
-              }}
+              style={
+                {
+                  flex: 1,
+                  height: 60,
+                  borderRadius: 35,
+                  justifyContent: "center",
+                  alignContent: "center",
+                  backgroundColor: "#2EA6FF",
+                  shadowOpacity: 1,
+                  shadowRadius: 8,
+                  shadowColor: "#2EA6FF",
+                  shadowOffset: { width: 0, height: 4 }
+                } // made up this //     numbers, as I can't replicate Sketch parameters
+              }
               onPress={this.openFilmSearchSection.bind(this)}
             >
               <Text
                 style={{
-                  flex: 1,
                   justifyContent: "center",
-                  alignContent: "center",
-                  color: "#F8F8F8"
+                  alignSelf: "center",
+                  color: "#F8F8F8",
+                  fontSize: 20
                 }}
               >
                 Consiglia serie TV o film
