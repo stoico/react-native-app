aee204376172b62f00ef83f7bc65fde6338c5b79
Carmine Di Martino <cardimart@gmail.com>
2019-01-13 10:03:21 +0100

    Set the font for the Suggestion button

--- components/FeedSuggestionBox.js
+++ components/FeedSuggestionBox.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Font, AppLoading } from "expo";
-import styled from "styled-components/native";
 
 export default class FeedSuggestionBox extends React.Component {
   constructor(props) {
--- components/SuggestionButton.js
+++ components/SuggestionButton.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Font, AppLoading } from "expo";
 
 export default class SuggestionButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fontLoaded: false };
+  }
+
   openFilmSearchSection = () => {
     console.log("\nJust testing the button\n");
   };
 
+  async componentDidMount() {
+    await Font.loadAsync({
+      "Gilroy Light": require("../assets/fonts/gilroy-light.otf"),
+      "Gilroy Extrabold": require("../assets/fonts/gilroy-extrabold.otf")
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
-    return (
-      <TouchableOpacity
-        style={styles.blueSuggestionButton}
-        onPress={this.openFilmSearchSection.bind(this)}
-      >
-        <Text style={styles.textOfBlueButton}>Consiglia serie TV o film</Text>
-      </TouchableOpacity>
-    );
+    if (!this.state.fontLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <TouchableOpacity
+          style={styles.blueSuggestionButton}
+          onPress={this.openFilmSearchSection.bind(this)}
+        >
+          <Text style={styles.textOfBlueButton}>Consiglia serie TV o film</Text>
+        </TouchableOpacity>
+      );
+    }
   }
 }
 
@@ -32,6 +50,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 }
   },
   textOfBlueButton: {
+    fontFamily: "Gilroy Extrabold",
     justifyContent: "center",
     alignSelf: "center",
     color: "#F8F8F8",
