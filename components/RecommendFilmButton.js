23992271167c9a9d5bf9dade95a1c2f1f71e53f5
Carmine Di Martino <cardimart@gmail.com>
2019-02-05 16:36:12 +0100

    Add states to RecommendFilm button. It can now tell whether a film has just been recommended or had previously been recommended.

--- components/RecommendFilmButton.js
+++ components/RecommendFilmButton.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Font, AppLoading } from "expo";
 import { database } from "../config/Firebase";
 import firebase from "firebase";
 
 import { isiOS } from "../constants/Platform";
+import { getLightEstimationEnabled } from "expo/build/AR";
 
 export default class RecommendFilmButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { fontLoaded: false, user: null };
+    this.state = {
+      fontLoaded: false,
+      user: null,
+      isAlreadyInDatabase: false,
+      recommendClicked: false
+    };
 
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ user });
     });
   }
 
-  openFilmSearchSection = () => {
+  onFilmButtonClick = () => {
     if (this.state.user) {
       this.saveRecommendation(
         this.state.user.uid,
@@ -25,6 +31,8 @@ export default class RecommendFilmButton extends React.Component {
         this.props.posterPath,
         this.props.mediaType
       );
+
+      this.setState({ recommendClicked: true });
     }
   };
 
@@ -34,32 +42,47 @@ export default class RecommendFilmButton extends React.Component {
       "Gilroy Extrabold": require("../assets/fonts/gilroy-extrabold.otf")
     });
     this.setState({ fontLoaded: true });
-  }
-
-  saveRecommendation(userID, filmID, title, posterPath, mediaType) {
-    const thisDateTime = Date.now();
-    let isAlreadyInDatabase = false;
-    console.log("mediaType: " + mediaType);
 
     firebase
       .database()
-      .ref("recommendations/" + userID)
+      .ref("recommendations/" + this.state.user.uid)
       .orderByChild("showID")
-      .equalTo(filmID)
+      .equalTo(this.props.filmID)
       .once("value")
       .then(snapshot => {
         if (snapshot.exists()) {
           console.log("exists");
           //TO FIX. Learn about callbacks and promises
           //This is called after the below code if (isAlreadyInDatabase)
-          isAlreadyInDatabase = true;
-          console.log(isAlreadyInDatabase);
+          this.setState({ isAlreadyInDatabase: true });
+          console.log(this.state.isAlreadyInDatabase);
         }
       });
+  }
 
-    console.log(isAlreadyInDatabase);
+  saveRecommendation(userID, filmID, title, posterPath, mediaType) {
+    const thisDateTime = Date.now();
+    console.log("mediaType: " + mediaType);
+
+    // firebase
+    //   .database()
+    //   .ref("recommendations/" + userID)
+    //   .orderByChild("showID")
+    //   .equalTo(filmID)
+    //   .once("value")
+    //   .then(snapshot => {
+    //     if (snapshot.exists()) {
+    //       console.log("exists");
+    //       //TO FIX. Learn about callbacks and promises
+    //       //This is called after the below code if (isAlreadyInDatabase)
+    //       this.setState({ isAlreadyInDatabase: true });
+    //       console.log(this.state.isAlreadyInDatabase);
+    //     }
+    //   });
+
+    console.log(this.state.isAlreadyInDatabase);
     // Need to add mediaType to the database
-    if (!isAlreadyInDatabase) {
+    if (!this.state.isAlreadyInDatabase) {
       firebase
         .database()
         .ref("recommendations/" + userID)
@@ -79,12 +102,22 @@ export default class RecommendFilmButton extends React.Component {
       return <AppLoading />;
     } else {
       return (
-        <TouchableOpacity
-          style={styles.blueSuggestionButton}
-          onPress={this.openFilmSearchSection.bind(this)}
-        >
-          <Text style={styles.textOfBlueButton}>Consiglialo</Text>
-        </TouchableOpacity>
+        <React.Fragment>
+          {this.state.isAlreadyInDatabase ||
+          this.state.recommendClicked ? null : (
+            <TouchableOpacity
+              style={styles.blueSuggestionButton}
+              onPress={this.onFilmButtonClick.bind(this)}
+            >
+              <Text style={styles.textOfBlueButton}>Consiglialo</Text>
+            </TouchableOpacity>
+          )}
+          {!this.state.recommendClicked ? null : (
+            <View style={styles.greenSuggestionButton}>
+              <Text style={styles.textOfBlueButton} />
+            </View>
+          )}
+        </React.Fragment>
       );
     }
   }
@@ -104,6 +137,19 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 8,
     shadowColor: "#2EA6FF",
+    shadowOffset: { width: 0, height: 4 }
+  },
+  greenSuggestionButton: {
+    flex: 1,
+    height: 60,
+    borderRadius: 35,
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "#8CD635",
+    elevation: 4,
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    shadowColor: "#8CD635",
     shadowOffset: { width: 0, height: 4 }
   },
   textOfBlueButton: {
